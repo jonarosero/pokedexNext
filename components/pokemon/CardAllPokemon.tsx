d6dc61b421d18db7d444e4a50c70920ed7f52566
jonarosero <jars9605@hotmail.com>
2023-04-07 20:34:24 -0500

    Feat: navbar is responsive

--- components/pokemon/CardAllPokemon.tsx
+++ components/pokemon/CardAllPokemon.tsx
@@ -1,51 +1,52 @@
-import { Button, Card, Col, Grid, Row, Text, useTheme } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Col,
+  Grid,
+  Row,
+  Text,
+  useTheme,
+} from "@nextui-org/react";
 import Link from "next/link";
 
-export const CardPokemons = ({pokemon, id}: any) =>{
-    const { theme } = useTheme();
-    
-    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+export const CardPokemons = ({ pokemon, id }: any) => {
+  const { theme } = useTheme();
 
-    return (
-      <Grid xs={12} md={4} xl={3} justify="center">
-        <Card key={pokemon.name} css={{ w: "100", h: "300px" }}>
-          <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-            <Col>
-              <Text
-                size={12}
-                weight="bold"
-                transform="uppercase"
-                color="#9E9E9E"
-              >
-                Atrápalos a todos
-              </Text>
-              <Text h2 size={14} b>
-                  N°: {id}
-                </Text>
-            </Col>
-          </Card.Header>
-          <Card.Body
-            css={{ p: 0, backgroundColor: theme?.colors.gray200.value }}
-          >
-            <Card.Image
-              src={imageUrl}
-              width="100%"
-              height="100%"
-              alt={pokemon.name}
-            />
-          </Card.Body>
-          <Card.Footer
-            isBlurred
-            css={{
-              position: "absolute",
-              bgBlur: "#0f111466",
-              borderTop: "$borderWeights$light solid $gray800",
-              bottom: 0,
-              zIndex: 1,
-            }}
-          >
-            <Row>
-              <Col span={8}>
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+
+  return (
+    <Grid xs={12} md={4} xl={3} justify="center">
+      <Card key={pokemon.name} css={{ w: "100", h: "300px" }}>
+        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+          <Col>
+            <Text size={12} weight="bold" transform="uppercase" color="#9E9E9E">
+              Atrápalos a todos
+            </Text>
+            <Text h2 size={14} b>
+              N°: {id}
+            </Text>
+          </Col>
+        </Card.Header>
+        <Card.Body css={{ p: 0, backgroundColor: theme?.colors.gray200.value }}>
+          <Card.Image
+            src={imageUrl}
+            width="100%"
+            height="100%"
+            alt={pokemon.name}
+          />
+        </Card.Body>
+        <Card.Footer
+          isBlurred
+          css={{
+            position: "absolute",
+            bgBlur: "#0f111466",
+            borderTop: "$borderWeights$light solid $gray800",
+            bottom: 0,
+            zIndex: 1,
+          }}
+        >
+          <Row>
+            <Col span={8}>
               <Text
                 h3
                 css={{
@@ -55,23 +56,21 @@ export const CardPokemons = ({pokemon, id}: any) =>{
               >
                 {pokemon.name}
               </Text>
-
-              </Col>
-              <Col>
-                <Row justify="flex-end">
+            </Col>
+            <Col>
+              <Row justify="flex-end">
+                <Link href={`/pokemon/${id}`} style={{ color: "white" }}>
                   <Button flat auto rounded color="gradient">
                     <Text size={12} weight="bold" transform="uppercase">
-                      <Link href={`/pokemon/${id}`}style={{ color: "white" }}>
-                        Ver más
-                      </Link>
+                      Ver más
                     </Text>
                   </Button>
-                </Row>
-              </Col>
-            </Row>
-          </Card.Footer>
-        </Card>
-      </Grid>
-    );
-  ;
-}
+                </Link>
+              </Row>
+            </Col>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </Grid>
+  );
+};
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,147 +1,274 @@
-import { Button, FormElement, Input, Navbar, Text, User } from "@nextui-org/react"
+import {
+    Avatar,
+  Button,
+  Dropdown,
+  FormElement,
+  Input,
+  Navbar,
+  Text,
+  User,
+} from "@nextui-org/react";
 import Image from "next/image";
 import Link from "next/link";
 import { login, logout, pokeApi, useAuth } from "../../api";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { Login, Logout } from "react-iconly";
+import { Login, Logout, Work } from "react-iconly";
+import NavbarLink from "@nextui-org/react/types/navbar/navbar-link";
 
-interface NavbarProps{
-    menu?: string,
+interface NavbarProps {
+  menu?: string;
 }
 
+export const NavbarPokedex = ({ menu }: NavbarProps) => {
+  const { user, loading, error } = useAuth();
 
-export const NavbarPokedex =({ menu }: NavbarProps)=>{
+  const handleLogin = async () => {
+    await login();
+  };
 
-    const {user, loading, error} = useAuth();
+  const handleLogout = async () => {
+    await logout();
+  };
 
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleLogin = async()=>{
-      await login();
-    }
-  
-    const handleLogout = async() =>{
-      await logout();
-    }
+  const router = useRouter();
 
+  async function handleSearchChange(event: React.ChangeEvent<FormElement>) {
+    const searchTerm = event.target.value;
+    setSearchTerm(searchTerm);
+  }
 
-    const [searchTerm, setSearchTerm] = useState("");
-
-    const router = useRouter();
-
-    async function handleSearchChange(event: React.ChangeEvent<FormElement>) {
-        const searchTerm = event.target.value;
-        setSearchTerm(searchTerm);
+  async function handleSearchEnter(
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) {
+    if (event.key === "Enter") {
+      const result = await searchPokemon(searchTerm);
+      if (result) {
+        router.push(`/pokemon/${result.id}`);
       }
-
-    async function handleSearchEnter(event: React.KeyboardEvent<HTMLInputElement>) {
-        if(event.key === "Enter"){
-            const result = await searchPokemon(searchTerm);
-            if(result){
-                router.push(`/pokemon/${result.id}`)
-            } 
-        }
-        
     }
+  }
 
-    let kanto;
-    let jhoto;
-    let hoenn;
-    
-    switch (menu) {
-        case "kanto":
-            kanto=true;
-            jhoto=false;
-            hoenn=false;
-            break;
+  let kanto;
+  let jhoto;
+  let hoenn;
 
-        case "johto":
-            kanto=false;
-            jhoto=true;
-            hoenn=false;
-            break;
+  switch (menu) {
+    case "kanto":
+      kanto = true;
+      jhoto = false;
+      hoenn = false;
+      break;
 
-        case "hoenn":
-            kanto=false;
-            jhoto = false;
-            hoenn = true;
-            break;
+    case "johto":
+      kanto = false;
+      jhoto = true;
+      hoenn = false;
+      break;
 
-        default:
-            kanto=false;
-            jhoto = false;
-            hoenn = false;
-            break;
-    }
+    case "hoenn":
+      kanto = false;
+      jhoto = false;
+      hoenn = true;
+      break;
 
-    
-    return (    
-        <Navbar isBordered variant='sticky'>
-            <Navbar.Brand css={{mr:"$4"}}>
-            <Link href="/">
-             <Image src={"/pokeball.png"}
-              alt="icono de la app"
-              width={20}
-              height={20}/></Link>
-              <Text b color="inherit" css={{mr:"$14"}} hideIn="xs">Pokédex</Text>
-              <Navbar.Content hideIn='xs' variant="highlight-rounded" activeColor="secondary" >
-                <Navbar.Link isActive={kanto} href="/">Kanto</Navbar.Link>
-                <Navbar.Link isActive={jhoto} href="/johto">Johto</Navbar.Link>
-                <Navbar.Link isActive={hoenn} href="/hoenn">Hoenn</Navbar.Link>
-            </Navbar.Content>
-            </Navbar.Brand>
-            <Navbar.Content css={{"@xsMax":{
-                w:"100%",
-                jc: "space-between",
+    default:
+      kanto = false;
+      jhoto = false;
+      hoenn = false;
+      break;
+  }
+
+  return (
+    <Navbar isBordered variant="sticky">
+                <Navbar.Toggle aria-label="toggle navigation" showIn={"xs"} />
+      <Navbar.Brand css={{ mr: "$4" }} hideIn="xs">
+        <Link href="/">
+          <Image
+            src={"/pokeball.png"}
+            alt="icono de la app"
+            width={20}
+            height={20}
+          />
+        </Link>
+        <Link href={"/"} style={{ color: "inherit" }}>
+          <Text b color="inherit" css={{ mr: "$14" }} >
+            Pokédex
+          </Text>
+        </Link>
+        <Navbar.Content
+          hideIn="xs"
+          variant="highlight-rounded"
+          activeColor="secondary"
+        >
+          <Navbar.Link isActive={kanto} href="/">
+            Kanto
+          </Navbar.Link>
+          <Navbar.Link isActive={jhoto} href="/johto">
+            Johto
+          </Navbar.Link>
+          <Navbar.Link isActive={hoenn} href="/hoenn">
+            Hoenn
+          </Navbar.Link>
+        </Navbar.Content>
+      </Navbar.Brand>
+      <Navbar.Content
+        css={{
+          "@xsMax": {
+            w: "100%",
+            jc: "space-between",
+          },
+        }}
+      >
+        <Navbar.Item
+          css={{
+            "@xsMax": {
+              w: "100%",
+              jc: "center",
             },
-            }}>
-                <Navbar.Item css={{
-                    "@xsMax":{
-                        w:"100%",
-                        jc:"center",
-                    },
-                }}>
-                    <Input clearable 
-                    
-                    css={{
-                      w: "100%",
-                      "@xsMax": {
-                        mw: "300px",
-                      },
-                      "& .nextui-input-content--left": {
-                        h: "100%",
-                        ml: "$4",
-                        dflex: "center",
-                      },
-                    }}
-                    placeholder="Buscar..."
-                    onChange={handleSearchChange}
-                    onKeyDown={handleSearchEnter} 
-                  />
+          }}
+        >
+          <Input
+            clearable
+            css={{
+              w: "100%",
+              "@xsMax": {
+                mw: "400px",
+              },
+              "& .nextui-input-content--left": {
+                h: "100%",
+                ml: "$4",
+                dflex: "center",
+              },
+            }}
+            placeholder="Buscar..."
+            onChange={handleSearchChange}
+            onKeyDown={handleSearchEnter}
+          />
+        </Navbar.Item>
+        <Navbar.Item>
+          {!user ? (
+            <Button
+              auto
+              onClick={handleLogin}
+              icon={<Login set="broken" primaryColor="white" />}
+            >
+              Iniciar Sesión
+            </Button>
+          ) : (
+            ""
+          )}
+        </Navbar.Item>
+        {user ? (
+          <Dropdown isBordered>
+            <Navbar.Item >
+              <Dropdown.Button
+                auto
+                light
+                css={{
+                  width: "200px",
+                  height: "100%",
+                  px: 0,
+                  dflex: "center",
+                  svg: { pe: "none" },
+                }}
+                ripple={false}
+              >
+                <User
+                  color="gradient"
+                  src={user.photoURL ?? undefined}
+                  name={user.displayName}
+                  description="Maestro Pokémon"
+                />
+              </Dropdown.Button>
+            </Navbar.Item>
+            <Dropdown.Menu
+              aria-label="Menu personal"
+              css={{
+                $$dropdownMenuWidth: "340px",
+                $$dropdownItemHeight: "70px",
+                "& .nextui-dropdown-item": {
+                  py: "$4",
+                  // dropdown item left icon
+                  svg: {
+                    color: "$secondary",
+                    mr: "$4",
+                  },
+                  // dropdown item title
+                  "& .nextui-dropdown-item-content": {
+                    w: "100%",
+                    fontWeight: "$semibold",
+                  },
+                },
+              }}
+            >
+              <Dropdown.Item
+                key="autoscaling"
+                showFullDescription
+                description="Revisa tu equipo de Pokemons"
+                icon={<Work set="broken" primaryColor="blueviolet" />}
+              >
+                Mi equipo
+              </Dropdown.Item>
+              <Dropdown.Item
+                key="usage_metrics"
+                showFullDescription
+                icon={<Logout set="broken" primaryColor="#F31260" />}
+              >
+                <Navbar.Link onClick={handleLogout}>Salir</Navbar.Link>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          ""
+        )}
+      </Navbar.Content>
+      <Navbar.Collapse>
+        <Navbar.CollapseItem>
+          <Link
+            style={{
+              color: "inherit",
+              minWidth: "100%",
+            }}
+            href="/"
+          >
+            Kanto
+          </Link>
+        </Navbar.CollapseItem>
+        <Navbar.CollapseItem>
+        <Link
+            style={{
+              color: "inherit",
+              minWidth: "100%",
+            }}
+            href="/johto"
+          >
+            Johto
+          </Link>
+        </Navbar.CollapseItem>
+        <Navbar.CollapseItem>
+        <Link
+            style={{
+              color: "inherit",
+              minWidth: "100%",
+            }}
+            href="/hoenn"
+          >
+            Hoenn
+          </Link>
+        </Navbar.CollapseItem>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-                </Navbar.Item>
-                <Navbar.Item>
-                    {!user? <Button auto onClick={handleLogin} icon={<Login set="broken" primaryColor="white"/>}>Iniciar Sesión</Button>:""}
-                </Navbar.Item>
-                <Navbar.Item>
-                    {user? <User bordered as="button" color="gradient" src = {user.photoURL ?? undefined} name={user.displayName} description="Maestro Pokémon"/>:""}            
-                </Navbar.Item>
-                <Navbar.Item>
-                {user? <Button auto onClick={handleLogout} icon={<Logout set="broken" primaryColor="white"/>}/>:""}
-                </Navbar.Item>
-
-            </Navbar.Content>
-
-        </Navbar>
-    )
-}
-
-async function searchPokemon(searchTerm: string){
-    try {
-        const response = await pokeApi.get(`/pokemon/${searchTerm.toLowerCase()}`);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        
-    }
+async function searchPokemon(searchTerm: string) {
+  try {
+    const response = await pokeApi.get(`/pokemon/${searchTerm.toLowerCase()}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
